47e8f827ab18da2f9bb0f1861849f0c71e65304f
bharath <bharathbk1221@gmail.com>
2023-03-29 14:57:51 +0530

    Api's

---
+++ app/admin/prescribtions.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Prescribtion do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :tablets, :appointment_id, :comments
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:tablets, :appointment_id, :comments]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
