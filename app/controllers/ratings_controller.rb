76faa856128e28603bedfcd21e8d351924fe5633
bharath <bharathbk1221@gmail.com>
2023-04-11 15:52:58 +0530

    Updated Views

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,6 @@
 class RatingsController < ApplicationController
   def index
-    if params[:ratable]=='hospitals'
+    if params[:ratable] == 'hospitals'
       @hospital = Hospital.find params[:ratable_id]
       @ratings = @hospital.ratings
     else
@@ -11,27 +11,31 @@ class RatingsController < ApplicationController
   end
 
   def create
-    if params[:ratable]=='hospitals'
-      @hospital = Hospital.find params[:ratable_id]
-      @rating =  @hospital.ratings.build rating_params
-      @rating.patient_id = current_account.accountable_id
-      if @rating.save
-        redirect_to hospital_ratings_path(params[:ratable_id]) , notice: "rating successfully created"
+    if current_account
+      if params[:ratable] == 'hospitals'
+        @hospital = Hospital.find params[:ratable_id]
+        @rating = @hospital.ratings.build rating_params
+        @rating.patient_id = current_account.accountable_id
+        if @rating.save
+          redirect_to hospital_ratings_path(params[:ratable_id]), notice: "rating successfully created"
+        end
+      elsif params[:ratable] == 'doctors'
+        @doctor = Doctor.find params[:ratable_id]
+        @rating = @doctor.ratings.build rating_params
+        @rating.patient_id = current_account.accountable_id
+        if @rating.save
+          redirect_to "/doctors/#{params[:ratable_id]}/ratings", notice: "rating successfully created"
+        else
+          redirect_to root_path , notice: "Not Allowed"
+        end
+        end
+      else
+        redirect_to hospitals_path , notice: "Login to Give a Rating"
       end
-    elsif   params[:ratable]== 'doctors'
-      @doctor = Doctor.find params[:ratable_id]
-      @rating = @doctor.ratings.build rating_params
-      @rating.patient_id = current_account.accountable_id
-      if @rating.save
-        redirect_to "/doctors/#{params[:ratable_id]}/ratings" , notice: "rating successfully created"
-      end
-
-    end
 
 
 
   end
-
 
   def destroy
     @ratings = Rating.find(params[:id])
@@ -39,8 +43,10 @@ class RatingsController < ApplicationController
 
     end
   end
+
   private
+
   def rating_params
-    params.require(:rating).permit(:rating , :review)
+    params.require(:rating).permit(:rating, :review)
   end
 end
