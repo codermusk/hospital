35fd6bdfaa5853b15f353401230c93cd22a395e1
bharath <bharathbk1221@gmail.com>
2023-04-14 16:18:20 +0530

    code fixes

--- app/admin/ratings.rb
+++ app/admin/ratings.rb
@@ -4,13 +4,12 @@ ActiveAdmin.register Rating do
     selectable_column
     id_column
     column  :ratable
-    column :patient
+    column "Reviewed by" ,  :patient
     column :rating
     column :review
   end
   filter  :rating ,label: "Rating Provided"
   filter :patient_id  , as: :select , collection: ->{Patient.all.collect  {|pat| [pat.name , pat.id]}}
-  # filter :ratable
   scope "Hospital" ,:get_ratings_hos
   scope "Doctor" ,:get_ratings_doc
 end
